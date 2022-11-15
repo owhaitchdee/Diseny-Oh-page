6c2f1ed15cfed46ce6dd0f391b05aba6a2d62873
owhaitchdee <108674156+owhaitchdee@users.noreply.github.com>
2022-11-15 11:48:20 -0800

    submit field in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,10 +6,8 @@ function Name() {
     <div className="container-fluid text-center" style={{backgroundColor:"black",opacity:"95%",textAlign:"center",whiteSpace:"nowrap",marginTop:"40px",paddingTop:"20px"}}>
         <p id="here" style={{color:"white"}}></p>
         <div style={{display: 'flex', justifyContent: 'center', verticalAlign:'bottom',paddingTop:'2px'}}>
-        <FloatingLabel  column sm={6} controlId="floatingInput" label="Name" className="mb-3 ml-3" >
-            <Form.Control id="input" type="text"  placeholder="Enter your name" style={{width:"400px"}}/>
-        </FloatingLabel>
-        <button className="btn-primary2" onClick={check} >Submit</button>
+            <input className="form-control me-2" type="search"  placeholder="Enter your name" style={{width:"400px"}}/>
+            <button className="btn-primary2" onClick={check} >Submit</button>
     </div></div>
   );
 }
