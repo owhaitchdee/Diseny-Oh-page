d981b280850eb71ecae304cfea1790b7c19540ff
owhaitchdee <108674156+owhaitchdee@users.noreply.github.com>
2022-11-14 21:06:49 -0800

    corrected form

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,14 @@ import Form from 'react-bootstrap/Form';
 
 function Name() {
   return (
-    <div className="container-fluid" style={{backgroundColor:"black",opacity:"95%",textAlign:"center",whiteSpace:"nowrap",marginTop:"40px",paddingTop:"20px"}}>
+    <div className="container-fluid text-center" style={{backgroundColor:"black",opacity:"95%",textAlign:"center",whiteSpace:"nowrap",marginTop:"40px",paddingTop:"20px"}}>
         <p id="here" style={{color:"white"}}></p>
-        <FloatingLabel  column sm={6} controlId="floatingInput" label="Name" className="mb-3 ml-3">
+        <div style={{display: 'flex', justifyContent: 'center', verticalAlign:'bottom',paddingTop:'2px'}}>
+        <FloatingLabel  column sm={6} controlId="floatingInput" label="Name" className="mb-3 ml-3" >
             <Form.Control id="input" type="text"  placeholder="Enter your name" style={{width:"400px"}}/>
         </FloatingLabel>
-        <button className="btn btn-primary" onClick={check}>Submit</button>
-    </div>
+        <button className="btn-primary2" onClick={check} >Submit</button>
+    </div></div>
   );
 }
 
@@ -17,13 +18,13 @@ function check(){
 
     let a = document.getElementById("input").value;
     if (a === ""){
-        document.getElementById("here").innerHTML = "Please enter your name"
+        document.getElementById("here").innerHTML = "Please enter your name";
         document.getElementById("input").innerHTML = ""
     } else if (a !== "" ){
-        document.getElementById("here").innerHTML = "Hello "+ a + ", thank you for subscribing!"
-    } else{
+        document.getElementById("here").innerHTML = "Hello "+ a + ", thank you for subscribing!";
+        document.getElementById("input").innerHTML = ""
 
-    }
+    } 
     }
 
 export default Name;
